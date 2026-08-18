64f83c863195b8c1a3d66b3ed20d094e8a45c251
robinPecheurBstorm <robin.pecheur@bstorm.be>
2026-08-18 12:07:50 +0200

    Restructure de mon premier composant

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
-import MessageDisplay from './components/MessageDisplay'
+import MessageDisplay from './components/MessageDisplay/MessageDisplay'
 
 export default function App() {
   const [count, setCount] = useState(0)
