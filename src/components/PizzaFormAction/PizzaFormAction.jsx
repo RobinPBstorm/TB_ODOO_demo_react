c04a9c5fbbcc0ff7c6f3fec61db0a3d053f31be4
robinPecheurBstorm <robin.pecheur@bstorm.be>
2026-08-19 14:35:13 +0200

    Proposition pour garder les valeurs par défaut

--- src/components/PizzaFormAction/PizzaFormAction.jsx
+++ src/components/PizzaFormAction/PizzaFormAction.jsx
@@ -1,20 +1,30 @@
-import { useActionState, useState } from "react"
+import { useActionState, useState, useEffect } from "react"
 
 // action avec useActionState
 async function exampleOrder(prevState, formData) {
     const sizePizza = formData.get("sizePizza");
     const typePizza = formData.get("typePizza");
 
-    if (typePizza ==="3" && sizePizza==="large") {
-        return {message: "Cette combinaison de choix n'est plus disponible"}
-    }
+    // simuler le temps d'attente
+    await new Promise((resolve) => setTimeout(resolve, 1000));
 
-    console.log(typePizza, sizePizza);
     // suite des opérations
-    return {message: "Commande s'est bien passé."}
+
+    if (typePizza ==="3" && sizePizza==="large") {
+        return {
+            sizePizza : sizePizza,
+            typePizza : typePizza, 
+            message : "Cette combinaison de choix n'est plus disponible" }
+    }
+    
+    return {
+        sizePizza : sizePizza,
+        typePizza : typePizza,
+        message : "Commande s'est bien passé."
+    }
 }
 
-export default function PizzaFormControl() {
+export default function PizzaFormAction() {
 
     const pizzas = [
         {
@@ -42,29 +52,37 @@ export default function PizzaFormControl() {
     //     return {message: "Commande s'est bien passé."}
     // }
 
-    
+    // mes valeurs par défaut
+    const initialValue = {
+        typePizza: "2",
+        sizePizza: "medium"
+    }
+    const [state, handleAction, isPending] = useActionState(exampleOrder, {...initialValue, message : ""});
+    // pour forcer le reset du select avec une nouvelle valeur par défaut
+    const [formKey, setFormKey] = useState(Date.now());
 
-    const [state, handleAction, isPending] = useActionState(exampleOrder, {message:""})
-
-    
+    // réagit à la mise à jour du state pour modifier la key
+    useEffect(() => {
+        setFormKey(Date.now());
+    }, [state]);
 
     return (
         // se base plus sur l'objet formData
         <form action={handleAction}>
             <label htmlFor="typePizza">Choississez votre pizza:</label>
-            <select name="typePizza" id="typePizza" defaultValue={"2"}>
+            <select name="typePizza" id="typePizza" key={formKey} defaultValue={state.typePizza ?? "2"}>
                 {pizzas.map(pizza => <option key={pizza.id} value={pizza.id}>{pizza.name}</option>)}
             </select><br />
 
             <label htmlFor="sizePizzaLarge">Large</label>
-            <input type="radio" name="sizePizza" value="large" id="sizePizzaLarge"/>
+            <input type="radio" name="sizePizza" value="large" id="sizePizzaLarge"  defaultChecked={state.sizePizza === "large"}/>
             <label htmlFor="sizePizzaMedium">Medium</label>
-            <input type="radio" name="sizePizza" value="medium" id="sizePizzaMedium" defaultChecked={true}/>
+            <input type="radio" name="sizePizza" value="medium" id="sizePizzaMedium" defaultChecked={state.sizePizza === "medium"}/>
             <label htmlFor="sizePizzaSmall">Small</label>
-            <input type="radio" name="sizePizza" value="small" id="sizePizzaSmall"/>
+            <input type="radio" name="sizePizza" value="small" id="sizePizzaSmall"  defaultChecked={state.sizePizza === "small"}/>
             <br />
 
-            <input type="submit" value="Commander"/>
+            <input type="submit" value="Commander" disabled={isPending}/>
 
             {state.message && (<p>{state.message}</p>)}
         </form>
